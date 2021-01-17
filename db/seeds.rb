645fa87bdb2f47b9cbd5b27faf24ea4818f9f00a
Maria Le <hp.mars.le@gmail.com>
2021-01-17 17:36:48 -0600

    Config api routes and seed db with a FavoritesRepo named Favorites

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,4 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  faves_repo = FavoritesRepo.create({ name: 'Favorites' })
