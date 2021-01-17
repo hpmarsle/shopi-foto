786ef933fe72d92bf5b12e12d74feb43d3f56d4b
Maria Le <hp.mars.le@gmail.com>
2021-01-17 14:48:31 -0600

    Add Bootstrap classes

--- shopi-foto-client/app.js
+++ shopi-foto-client/app.js
@@ -10,16 +10,18 @@ class Gallery{
     }
 
     getPhotos() {
-        for(let i = 0; i < 6; i++){
-            
+        let r = document.getElementsByClassName('row')[0]
+        for(let i = 0; i < 6; i++){ 
             let photoCard = document.createElement('div')
             photoCard.classList.add('photo-card')
-            photoCard.innerHTML = this.photos[i] + i
+            photoCard.classList.add('col-xs-6')
+            photoCard.classList.add('col-md-4')
+            photoCard.innerHTML = this.photos[i]
             let fave_button = document.createElement('div')
             fave_button.classList.add('fave-button')
             fave_button.innerHTML = "<button class='fave-button'>Favorite</button>"
             photoCard.appendChild(fave_button)
-            this.gallery.appendChild(photoCard)
+            r.appendChild(photoCard)
         }
     }
 }
